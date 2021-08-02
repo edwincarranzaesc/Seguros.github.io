590d5a7e90d4b04e6e07eda7dc3596abb00ad34b
edwincarranzaesc <88289569+edwincarranzaesc@users.noreply.github.com>
2021-08-01 23:34:07 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,5 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    
+     apiKey: "AIzaSyCfDF5hrUCG3zxpmPB01Ngd2AmCVk3eAWY",
+    authDomain: "segurosed-48e1c.firebaseapp.com",
+    projectId: "segurosed-48e1c",
+    storageBucket: "segurosed-48e1c.appspot.com",
+    messagingSenderId: "316147527030",
+    appId: "1:316147527030:web:492de1bac0aa9e5232de57",
+    measurementId: "G-4VN1H724DF"
 });
